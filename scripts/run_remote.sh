9c6aaccba8ad3bf41cd3655c0072aa95b95323ac
Gaubert Santiago <gaubert.vinicius@gmail.com>
2026-01-12 23:05:57 -0800

    Sync + build + run from within remote

--- scripts/run_remote.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-BIN=${BUILD_DIR}/runtime
-
-ssh ${REMOTE_HOST} <<EOF
-  set -e
-  ${BIN}
-EOF
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,6 +4,5 @@ set -euo pipefail
 rsync -az --delete \
   --exclude build \
   --exclude .git \
-  --exclude .vscode \
   ./ \
   ${REMOTE_HOST}:${REMOTE_DIR}
