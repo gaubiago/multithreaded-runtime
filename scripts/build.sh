3902b653a09b46a99332496a501cc6f833a7f14d
Gaubert Santiago <gaubert.vinicius@gmail.com>
2026-01-14 22:10:55 -0800

    Remove unknown flag left by mistake

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cmake -S . -D -B build \
+cmake -S . -B build \
   -DCMAKE_TOOLCHAIN_FILE=cmake/clang-toolchain.cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
