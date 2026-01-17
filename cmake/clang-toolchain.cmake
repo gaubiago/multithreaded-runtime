e1bd0721f9a631587b106fd3644a468203db1499
Gaubert Santiago <gaubert.vinicius@gmail.com>
2026-01-16 21:51:58 -0800

    Add .clang-format and .clang-tidy

--- cmake/clang-toolchain.cmake
+++ cmake/clang-toolchain.cmake
@@ -1,6 +1,7 @@
 set(CMAKE_C_COMPILER clang CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS "-stdlib=libc++" CACHE STRING "" FORCE)
+# Explicitly add libc++ include path so clang-tidy and clang find standard headers
+set(CMAKE_CXX_FLAGS "-stdlib=libc++ -isystem /usr/lib/llvm-14/include/c++/v1" CACHE STRING "" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++ -lc++abi" CACHE STRING "" FORCE)
 set(CMAKE_CXX_STANDARD_LIBRARIES "-stdlib=libc++ -lc++abi" CACHE STRING "" FORCE)
 
